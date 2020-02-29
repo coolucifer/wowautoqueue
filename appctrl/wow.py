c23032b9c1858b834d4c102034682b960abbd62c
victor <1045606768@qq.com>
2020-02-29 14:59:23 +0800

    添加注释及分辨率适配

--- appctrl/wow.py
+++ appctrl/wow.py
@@ -7,15 +7,17 @@ from utils.screenshot import regionMatchesColor
 class WowClassicApp(AppCtrl):
     title = "魔兽世界"
 
-    def __init__(self, hWnd):
-        # super().__init__()
-        self.hWnd = hWnd
+    def __init__(self, hWnd = None):
+        super().__init__()
+        if (hWnd is not None):
+            self.hWnd = hWnd
 
     def isCharacterSelect(self):
         """Test if in Character Select screen.
         """
+        # 只适合16:9分辨率
         rect = self.getRect()
-        return regionMatchesColor((rect[0] + 857, rect[1] + 983, 22, 19), (117, 0, 0), 16)
+        return regionMatchesColor((rect[0] + ((rect[2] - rect[0]) / 1936) * 865, rect[1] + ((rect[3] - rect[1]) / 1119) * 1024, 10, 10), (120, 17, 17), 16)
 
     def isLoginScreen(self):
         # 1920x1080窗口, (20, 60, 30, 70)区域内颜色为#000
@@ -25,7 +27,7 @@ class WowClassicApp(AppCtrl):
     def enterGame(self):
         self.activate()
         rect = self.getRect()
-        gui.moveTo(rect[0] + 860, rect[1] + 990, 1)
+        gui.moveTo(rect[0] + ((rect[2] - rect[0]) / 1936) * 860, rect[1] + ((rect[3] - rect[1]) / 1119) * 990, 1)
         gui.click()
 
     def antiIdle(self):
