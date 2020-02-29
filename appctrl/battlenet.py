d4d2f19ad33a7266fa7894b4a6ed137e1c98a745
victor <1045606768@qq.com>
2020-02-29 14:15:36 +0800

    添加选择wow账号方法

--- appctrl/battlenet.py
+++ appctrl/battlenet.py
@@ -18,3 +18,23 @@ class BattleNetApp(AppCtrl):
         gui.click()
         gui.moveTo(100, 100)
 
+    def select_account(self, index):
+        # 选择从最下往上数第x个wow账号
+        hWnd = self.getWindow()
+        rect = self.getRect()
+
+        # wow选择按钮位置
+        account_btn_pos = (rect[0] + 475, rect[3] - 130)
+        # wow_x 位置
+        # 从下往上数, 第一个wow y是 rect3 - 170, 第二个wow y是 rect3 - 200
+        # 账号只有两个wow, 多出来的修改对应值
+        wow_x_pos = (rect[0] + 475, rect[3] - (140 + 30 * index))
+
+        self.activate()
+        gui.moveTo(*account_btn_pos, 1)
+        gui.click()
+        gui.moveTo(*wow_x_pos, .5)
+        gui.click()
+
+
+
